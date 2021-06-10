517800dcbf6808879b205d8db28c0e3cea069d92
aishdharan <aishwaryadharan@gmail.com>
2021-06-11 02:33:34 +0530

    Random range number counter

---
+++ week3/problem3.py
@@ -0,0 +1,20 @@
+import os
+import sys
+import random
+
+
+def main():
+    x = random.choices(range(5), k=20)
+    y = x.count(0)
+    z = x.count(1)
+    w = x.count(2)
+    s = x.count(3)
+    t = x.count(4)
+    print(f'x = {x}')
+    print(f'no. of zeroes = {y}, no. of ones = {z}, no. of twos = {w}, no. of threes = {s}, no. of fours = {t}')
+
+    return os.X_OK
+
+
+if __name__ == "__main__":
+    sys.exit(main())
