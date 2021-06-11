2571f0c2a64639a0d331985013e84107f007b70b
aishdharan <aishwaryadharan@gmail.com>
2021-06-11 16:55:25 +0530

    Changed variables

--- week3/problem3.py
+++ week3/problem3.py
@@ -10,13 +10,13 @@ Notes:
 
 def main():
     x = random.choices(range(5), k=20)
-    y = x.count(0)
-    z = x.count(1)
-    w = x.count(2)
-    s = x.count(3)
-    t = x.count(4)
+    x0 = x.count(0)
+    x1 = x.count(1)
+    x2 = x.count(2)
+    x3 = x.count(3)
+    x4 = x.count(4)
     print(f'x = {x}')
-    print(f'no. of zeroes = {y}, no. of ones = {z}, no. of twos = {w}, no. of threes = {s}, no. of fours = {t}')
+    print(f'no. of zeroes = {x0}, no. of ones = {x1}, no. of twos = {x2}, no. of threes = {x3}, no. of fours = {x4}')
 
     return os.X_OK
 
