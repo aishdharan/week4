e06e6dee02647f61af3a4ba134d6b8e81d38e475
aishdharan <aishwaryadharan@gmail.com>
2021-06-13 02:04:15 +0530

    used step argument to reverse range

--- week3/problem9.py
+++ week3/problem9.py
@@ -9,10 +9,11 @@ Revisit the notes with regard to slicing
 
 
 def main():
-    x = list(range(20, 41))
+    x = list(range(12, 2, -1))
     # don't use slices for this
     # print(x[::-1])
     # I'm confident you can figure it out
+    print(f"x reversed = {x}")
 
     return os.X_OK
 
