bc3a1a233d7d7f291ad08d7e93e350bf45aad394
aishdharan <aishwaryadharan@gmail.com>
2021-06-14 19:00:30 +0530

    Separated characters given by user in form of list

---
+++ week4/problem0.py
@@ -0,0 +1,22 @@
+import os
+import sys
+
+
+def main():
+    ur_char = input("Give 10 character words: ")
+    if len(ur_char) < 10:
+        print("Add more characters")
+    elif len(ur_char) > 10:
+        print("Please add 10 characters only.")
+    ur_list = list(ur_char)
+    print(ur_list)
+
+    for k in iter(ur_list):
+        print(f"key {k}")
+    # I'm not getting how to count the characters the user puts
+    # Should I let the program count the number of characters the user gives whether its > or < 10?
+    return os.X_OK
+
+
+if __name__ == '__main__':
+    sys.exit(main())
