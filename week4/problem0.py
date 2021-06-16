ce456b99f9b7490f413507b2983a5630e6fd817b
paulkorir <paul@sci2pro.com>
2021-06-16 06:11:50 +0100

    notes and scores

--- week4/problem0.py
+++ week4/problem0.py
@@ -1,9 +1,23 @@
 import os
 import sys
 
+"""
+Notes:
+- Excellent solution! I would not have thought of that but your way is 
+perhaps the best I have seen ever. I would have done:
+
+usr_dict = dict()
+for k in usr_char:
+    if k not in usr_dict:
+        usr_dict[k] = 1
+    else:
+        usr_dict[k] += 1
+"""
+
 
 def main():
     usr_char = input("Give 10 character words: ")
+    # fixme: consider using a try/except block here
     if len(usr_char) < 10:
         print("Add more characters")
     elif len(usr_char) > 10:
@@ -11,6 +25,7 @@ def main():
     else:
         print("10 characters added")
     usr_dict = dict()
+    # fixme: you can run a for loop over a string without the need to pass it to iter()
     for k in iter(usr_char):
         print(f"key {k}")
         usr_dict[k] = usr_dict.get(k, 0) + 1
