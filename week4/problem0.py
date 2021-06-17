c0120603d76dc6facba06de0fa329cdb29d75fec
paulkorir <paul@sci2pro.com>
2021-06-17 05:32:47 +0100

    notes on problem0.py

--- week4/problem0.py
+++ week4/problem0.py
@@ -3,6 +3,11 @@ import sys
 
 """
 Notes:
+- Some of the constraints in the challenges are only artificial to stretch you. 
+Remember your goal is to be able to write code exactly to your desired outcome
+and this usually entails having a constraint. In my experience, it is very
+helpful to separate the requirements from the task of programming since as you
+get sucked into solving the problem your focus gets redirected to minutiae.
 - Excellent solution! I would not have thought of that but your way is 
 perhaps the best I have seen ever. I would have done:
 
@@ -17,20 +22,21 @@ for k in usr_char:
 
 def main():
     usr_char = input("Give word of 10 characters: ")
-    # fixme: consider using a try/except block here
     try:
         assert len(usr_char) == 10
     except AssertionError:
-        print("Give 10 characters only")
+        # see https://github.com/sci2pro/pl1c-solutions
+        # we write to sys.stderr for errors/warnings
+        print("Give 10 characters only", file=sys.stderr)
+        # terminate; user did not follow instructions
+        return os.EX_USAGE
+    # no need really since we know they should be 10
     print(f"No. of characters given: {len(usr_char)}")
     usr_dict = dict()
-    # fixme: you can run a for loop over a string without the need to pass it to iter()
     for k in usr_char:
         # print(f"key {k}")
         usr_dict[k] = usr_dict.get(k, 0) + 1
-
     print(f"count of all characters: {usr_dict}")
-
     return os.X_OK
 
 
