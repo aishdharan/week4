d4fbe0aaf7d0d89c99ae43e079c5ab6d7a38d59c
aishdharan <aishwaryadharan@gmail.com>
2021-06-21 04:33:28 +0530

    determined names of unprocessed files

---
+++ week4/problem6.py
@@ -0,0 +1,32 @@
+import sys
+import random
+import string
+
+
+def main():
+    files = [f"file_{''.join(random.choices(string.ascii_lowercase, k=6))}.pdf" for _ in range(100)]
+    # files that have been completely processed
+    processed = dict(
+        zip(
+            random.choices(files, k=10),
+            random.choices(range(1, 100), k=10)
+        )
+    )
+    set_processed = set(processed)
+    print(set_processed)
+    # files currently processing
+    processing = random.choices(list(set(files).difference(processed.keys())), k=15)
+    #print(processing)
+    set_processing = set(processing)
+    print(set_processing)
+    unprocessed = set_processing.difference(set_processed)
+    print(unprocessed)
+    # files yet to be processed
+    # unprocessed = None
+
+
+    return 0
+
+
+if __name__ == "__main__":
+    sys.exit(main())
