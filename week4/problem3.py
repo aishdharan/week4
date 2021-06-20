b4b57e63e53504215e3ad0a5d4e1345f0a715bf7
aishdharan <aishwaryadharan@gmail.com>
2021-06-21 00:57:07 +0530

    Combined two dictionaries in one by adding values for common keys

---
+++ week4/problem3.py
@@ -0,0 +1,29 @@
+import random
+import sys
+import string
+
+
+def main():
+    v1 = random.choices(range(101), k=100)
+    lower_alpha = string.ascii_lowercase
+    random_letter = random.choices(lower_alpha, k=100)
+    v2 = random.choices(range(101), k=100)
+    lower_alpha2 = string.ascii_lowercase
+    random_letter2 = random.choices(lower_alpha2, k=100)
+    d1 = dict(zip(random_letter, v1))
+    d2 = dict(zip(random_letter2, v2))
+    print("d1 = ", d1)
+    print("d2 = ", d2)
+    d3 = dict(d1)
+    d3.update(d2)
+    for i, j in d1.items():
+        for x, y in d2.items():
+            if i == x:
+                d3[i] = (j + y)
+    print(f"d1 + d2 = {d3}")
+
+    return 0
+
+
+if __name__ == "__main__":
+    sys.exit(main())
